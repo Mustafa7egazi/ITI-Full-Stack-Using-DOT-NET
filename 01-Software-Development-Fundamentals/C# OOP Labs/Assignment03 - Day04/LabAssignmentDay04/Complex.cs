e579b455cef5297ed3279f58e74fc912032eb4da
7egz <mustafareda3210@gmail.com>
2025-03-22 17:42:59 +0200

    Submitted 3rd C# Lab

---
+++ 01-Software-Development-Fundamentals/C# OOP Labs/Assignment03 - Day04/LabAssignmentDay04/Complex.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAssignmentDay03
+{
+    public class Complex
+    {
+
+        private int real;
+        private int img;
+
+        #region Simple Normal Property
+        public int Real { get; set; }
+        public int Img { get; set; }
+        #endregion
+
+        #region Spreaded Property if validation needed
+        //public int Real
+        //{
+        //    get
+        //    {
+        //        return real;
+        //    }
+        //    set
+        //    {
+        //        real = value;
+        //    }
+        //}
+        //public int Img
+        //{
+        //    get
+        //    {
+        //        return img;
+        //    }
+        //    set
+        //    {
+        //        img = value;
+        //    }
+        //}
+        #endregion
+
+        #region Parameterless CTOR
+        public Complex()
+        {
+            real = 3;
+            img = 4;
+        }
+        #endregion
+
+        #region Parameterized CTOR - used this keyword inside also
+        public Complex(int real, int img)
+        {
+            this.real = real;
+            this.img = img;
+        }
+        #endregion
+
+        #region Using this KEYWORD inside Adding method
+        public Complex Add(Complex right)
+        {
+            Complex result = new Complex();
+            result.real = this.real + right.real;
+            result.img = this.img + right.img;
+            return result;
+        }
+        #endregion
+
+
+        public string Print()
+        {
+
+            if (img < 0 && img != -1 && real != 0)
+            {
+                return $"{real}{img}i";
+            }
+            else if (img < 0 && img != -1 && real == 0)
+            {
+                return $"{img}i";
+            }
+            else if (img < 0 && img == -1 && real == 0)
+            {
+                return $"-i";
+            }
+            else if (img == 0 && real != 0)
+            {
+                return $"{real}";
+            }
+            else if (real == 0 && img != 0 && img != 1)
+            {
+                return $"{img}i";
+            }
+            else if (real == 0 && img == 1)
+            {
+                return $"i";
+            }
+            else if (img == 1)
+            {
+                return $"{real}+i";
+            }
+            else if (img == -1)
+            {
+                return $"{real}-i";
+            }
+            else if (real == 0 && img == 0)
+            {
+                return "0";
+            }
+            else
+            {
+                return $"{real}+{img}i";
+            }
+
+        }
+
+    }
+}
+
---
+++ 01-Software-Development-Fundamentals/C# OOP Labs/Assignment03 - Day04/LabAssignmentDay04/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LabAssignmentDay04
+{
+    using System;
+
+    class Program
+    {
+        static void ModifyByValue(int[] numbers)
+        {
+            numbers[0] = 100;
+            numbers = new int[5];
+            numbers[0] = 200;
+        }
+
+        static void ModifyByReference(ref int[] numbers)
+        {
+            numbers[0] = 100;
+            numbers = new int[5];
+            numbers[0] = 200;
+        }
+
+        static void Main()
+        {
+            // Test passing by value 
+            int[] array1 = new int[5] { 1, 2, 3, 4, 5 };
+            ModifyByValue(array1);
+            Console.WriteLine("By Value: " + array1[0]); // Output: 100
+
+            // Test passing by reference
+            int[] array2 = new int[5] { 1, 2, 3, 4, 5 };
+            ModifyByReference(ref array2);
+            Console.WriteLine("By Reference: " + array2[0]); // Output: 200
+        }
+    }
+}
