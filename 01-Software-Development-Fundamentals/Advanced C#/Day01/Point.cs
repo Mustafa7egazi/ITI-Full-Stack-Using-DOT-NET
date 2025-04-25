8fe6a2bad478e8afc3030323a5e865532c8f5dc4
7egz <mustafareda3210@gmail.com>
2025-04-25 16:29:59 +0300

    Submitted 1st Lab | Adv C#

---
+++ 01-Software-Development-Fundamentals/Advanced C#/Day01/Point.cs
@@ -0,0 +1,59 @@
+﻿namespace Day01
+{
+    internal class Point
+    {
+        int x;
+        int y;
+        int z;
+
+
+        public int X {
+        
+            get { return x; }
+            set { x = value; }
+
+        }
+        public int Y { 
+        
+            get { return y; }
+            set { y = value; }
+
+        }
+
+        public int Z { 
+        
+            get { return z; }
+            set { z = value; }
+        
+        }
+
+        public Point()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+           
+        }
+
+
+        public Point(int firstD , int secondD , int thirdD)
+        {
+            X = firstD;
+           Y = secondD; 
+            Z = thirdD;
+
+           
+        }
+
+        public override string ToString()
+        {
+           return $"Point coordinates: ({X},{Y},{Z})";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Point receivedPoint = obj as Point;
+            return x == receivedPoint.x && y == receivedPoint.y && z == receivedPoint.z;
+        }
+    }
+}
---
+++ 01-Software-Development-Fundamentals/Advanced C#/Day01/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Day01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter first point coordinates speprated by space: ");
+            string []firstCoords = Console.ReadLine().Split();
+
+            Point p1 = new Point(int.Parse(firstCoords[0])
+                ,int.Parse(firstCoords[1])
+                ,int.Parse(firstCoords[2]));
+
+
+            Console.Write("Enter second point coordinates speprated by space: ");
+            string[] secondCoords = Console.ReadLine().Split();
+
+
+            Point p2 = new Point(int.Parse(secondCoords[0]),
+               int.Parse(secondCoords[1]),
+                int.Parse(secondCoords[2]));
+
+
+            Point[] ourPoints = [p1, p2];
+
+
+            Console.WriteLine($"First coords: {ourPoints[0]}");
+            Console.WriteLine($"Second coords: {ourPoints[1]}");
+
+            if (p1.Equals(p2))
+            {
+                Console.WriteLine("Are Equals");
+            }
+            else
+            {
+                Console.WriteLine("Are not Equals");
+            }
+
+        }
+    }
+}
